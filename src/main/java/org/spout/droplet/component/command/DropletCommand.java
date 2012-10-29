5e87055e6557447921050ddbefc442d9e9131396
Wulfspider <hello@wulfspider.com>
2012-10-29 01:18:42 -0400

    Changed license to MIT. Cleaned up pom.xml and properties.yml. Updated license header.txt template and readme. Updated license headers in source. Refactored to match other Droplets. Could use some more cleanup.

--- src/main/java/org/spout/droplet/component/command/DropletCommand.java
+++ src/main/java/org/spout/droplet/component/command/DropletCommand.java
@@ -24,33 +24,7 @@
  * License and see <http://www.spout.org/SpoutDevLicenseV1.txt> for the full license,
  * including the MIT license.
  */
-/*
- * This file is part of DropletEntity.
- *
- * Copyright (c) 2012, SpoutDev <http://www.spout.org/>
- * DropletEntity is licensed under the SpoutDev License Version 1.
- *
- * DropletEntity is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * In addition, 180 days after any changes are published, you can use the
- * software, incorporating those changes, under the terms of the MIT license,
- * as described in the SpoutDev License Version 1.
- *
- * DropletEntity is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License,
- * the MIT license and the SpoutDev License Version 1 along with this program.
- * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
- * License and see <http://www.spout.org/SpoutDevLicenseV1.txt> for the full license,
- * including the MIT license.
- */
-package org.spout.droplet.command;
+package org.spout.droplet.component.command;
 
 import org.spout.api.command.CommandContext;
 import org.spout.api.command.CommandSource;
@@ -58,12 +32,12 @@ import org.spout.api.command.annotated.Command;
 import org.spout.api.command.annotated.NestedCommand;
 import org.spout.api.exception.CommandException;
 
-import org.spout.droplet.DropletComponentPlugin;
+import org.spout.droplet.component.DropletComponent;
 
 public class DropletCommand {
-	private final DropletComponentPlugin plugin;
+	private final DropletComponent plugin;
 
-	public DropletCommand(DropletComponentPlugin plugin) {
+	public DropletCommand(DropletComponent plugin) {
 		this.plugin = plugin;
 	}
 
